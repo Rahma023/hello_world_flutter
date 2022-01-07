e50f3f0fb0f8e678e211b71a9a7431287551015c
Rahma023 <rahmafaizyusuf@gmail.com>
2022-01-07 14:52:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HelloWorld());
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
